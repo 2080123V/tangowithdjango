7d3687bf6a1bb102d3d93874a13c5337f3083ab0
2080123V <2080123V@student.gla.ac.uk>
2015-01-22 19:09:57 +0000

    Chapter 5 Completed

---
+++ Rango/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse                               #Each function is linked to a different url pattern
+                                                                   #based on its function name.
+def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/index.html', context_dict)
+    return HttpResponse("Rango says hey there world")
+def about(request):
+     # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Chilling in the waters of the fountain show:)"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/about.html', context_dict)
+    return HttpResponse("This tutorial has been put together by Rajeevan Vijayakumar, 2080123")
