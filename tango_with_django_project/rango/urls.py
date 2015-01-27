4e1d1922f73c9f73a05faa77225be793734c4b2e
2080123V <2080123V@student.gla.ac.uk>
2015-01-27 21:07:44 +0000

    Chapter 7 Completed(ish)

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,9 +4,8 @@ from rango import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),            #Links to the views file and links
-                       url(r'^about$', views.about, name='about')        #each url pattern with the correct name function.
-
-)
+                       url(r'^about$', views.about, name='about'),        #each url pattern with the correct name function.
+                       url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
 
 
 
