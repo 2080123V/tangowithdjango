a79bb78544453beb1ce1816d7e0fc14785479d36
2080123V <2080123V@student.gla.ac.uk>
2015-02-03 18:48:37 +0000

    Chapter 8 Completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -5,6 +5,8 @@ from rango import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),            #Links to the views file and links
                        url(r'^about$', views.about, name='about'),        #each url pattern with the correct name function.
+                       url(r'^add_category/$', views.add_category, name='add_category'),
+                       url(r'^add_page/$', views.add_page, name='add_page'),
                        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
 
 
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 from django.http import HttpResponse                               #Each function is linked to a different url pattern
                                                                    #based on its function name.
@@ -10,15 +12,15 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'pages':page_list, 'categories': category_list}
 
     return render(request, 'rango/index.html', context_dict)
     return HttpResponse("Rango says hey there world")
 def about(request):
      # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Chilling in the waters of the fountain show:)"}
+    context_dict = {'boldmessage': "It's me"}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -55,3 +57,53 @@ def category(request, category_name_slug):
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
