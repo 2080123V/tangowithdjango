70c5fc9cb9637590ea6d5ffd37b35edb5d161e90
2080123V <2080123V@student.gla.ac.uk>
2015-02-17 13:10:36 +0000

    Chapter 11 Completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,30 +7,61 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 from django.http import HttpResponse, HttpResponseRedirect         #Each function is linked to a different url pattern
                                                                    #based on its function name.
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'pages':page_list, 'categories': category_list}
 
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
     return render(request, 'rango/index.html', context_dict)
     return HttpResponse("Rango says hey there world")
 def about(request):
      # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = {'boldmessage': "It's me"}
-
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
 
-    return render(request, 'rango/about.html', context_dict)
+	# If the visits session varible exists, take it and use it.
+    # If it doesn't, we haven't visited the site so set the count to zero.
+    if request.session.get('visits'):
+       count = request.session.get('visits')
+    else:
+       count = 0
+    
+	return render(request, 'rango/about.html',{'visits': count})
     return HttpResponse("This tutorial has been put together by Rajeevan Vijayakumar, 2080123")
 
 def category(request, category_name_slug):
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = 'i&9c0qm8yi@&vou0(4o2r8e&k6k&+da8^w#20fv30t8tv6d0*m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
