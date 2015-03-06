2c959643df57c4132065f07f9ab84c09f20b7c54
2080123V <2080123V@student.gla.ac.uk>
2015-03-06 14:52:54 +0000

    Chapter 16 & 17 Completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,8 @@
 __author__ = 'Rajee'
 from django.conf.urls import patterns, url
 from rango import views
+from registration.backends.simple.views import RegistrationView
+
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),            #Links to the views file and links
@@ -9,6 +11,9 @@ urlpatterns = patterns('',
                        url(r'^add_page/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
                        url(r'^restricted/', views.restricted, name='restricted'),
                        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
-                       url(r'^search/', views.search, name='search'),)
+                       url(r'^search/', views.search, name='search'),
+					   url(r'^goto/', views.track_url, name='goto'),
+					   url(r'^add_profile/', views.register_profile, name='add_profile'),)
+					   
 
 
